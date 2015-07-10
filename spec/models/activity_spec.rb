9ff58e5ef2473c3b0e84f8d9ba1631423ac2de12
stephyc93 <stephyc93@gmail.com>
2015-07-09 20:03:29 -0600

    tested models activity_spec.rb file

--- spec/models/activity_spec.rb
+++ spec/models/activity_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Activity, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @activity = Activity.create!(name: 'Band')
+  end
+
+  it "returns the activity name" do
+    @activity.name
+    expect(@activity.name).to eq("#{@activity.name}")
+  end
+
 end
