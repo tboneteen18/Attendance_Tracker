9c16359fc54b1bdda087631b2284b6e463920113
stephyc93 <stephyc93@gmail.com>
2015-07-10 15:54:48 -0600

    all models are pretested

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Student, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe Student do
+
+  end
+  
 end
--- spec/models/teacher_spec.rb
+++ spec/models/teacher_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Teacher, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe Teacher do
+
+  end
+
 end
